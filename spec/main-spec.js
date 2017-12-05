61f39fbe1949c38308e2680850946a2c40ade037
Your Name <you@example.com>
2017-12-05 18:54:46 +0800

    add two ceshi

--- spec/main-spec.js
+++ spec/main-spec.js
@@ -7,9 +7,19 @@ describe(" gess-number",function(){
         let result = main(str);
         expect(result).to.equal('4A0B');
     });
-    it("get some correct answers",function(){
+    it("get all correct numbers but wrong station",function(){
         let str = '4321';
         let result = main(str);
         expect(result).to.equal('0A4B');
+    });
+    it("get two correct numbers and correct station",function(){
+        let str = '5634';
+        let result = main(str);
+        expect(result).to.equal('2A0B');
+    });
+    it("get all correct numbers but only two correct station",function(){
+        let str = '1243';
+        let result = main(str);
+        expect(result).to.equal('2A2B');
     });
 });
